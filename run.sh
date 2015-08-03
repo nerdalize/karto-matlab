21b6897fe27a48f9ad2c9ed179804f37b92fe709
Ad van der Veer <a.vanderveer@nerdalize.com>
2015-08-03 15:17:34 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+exec=$(find /job/ -name "run_*.sh" -print0)
+echo "Found run script '$exec'!" 
+if [ -z "$exec" ]; then
+	echo "Could not find runnable matlab script matching 'run_*.sh', exiting"
+	exit 1
+fi
+
+echo "Running '$exec' wit args '$@'"
+$exec /usr/local/MATLAB/MATLAB_Runtime/v85 $@
